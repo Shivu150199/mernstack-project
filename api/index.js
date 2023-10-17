783058849adc808ff71c80cc4432d4425c1e4e1e
shivam singh gautam <shivamsinghgautam397@gmail.com>
2023-10-17 23:41:38 +0530

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
-import express from 'express'
-
+import express  from "express";
+import dotenv from 'dotenv';
+import mongoose from 'mongoose'
 const app=express()
+dotenv.config();
 
 
-
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected to mongodb')
+}).catch((err)=>{
+    console.log(err)
+})
 
 
 
